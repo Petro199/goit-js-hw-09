26f21ca551f3bc19bf42c3f3335f086a0a6fed86
Petro199 <kryvyak2710@gmail.com>
2022-11-20 02:03:12 -0500

    update task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,14 @@
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+ const newPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(position);
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return newPromise;
 }
