0550a82c4b7ebbb1fb5b8be7b808fa680c858067
Petro199 <kryvyak2710@gmail.com>
2022-11-20 02:16:00 -0500

    task 3 update 123

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,14 +1,42 @@
-function createPromise(position, delay) {
+import Notiflix from 'notiflix';
+function createPromise(position, delayN) {
   const shouldResolve = Math.random() > 0.3;
  const newPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log(position);
       if (shouldResolve) {
-        resolve({ position, delay });
+        resolve({ position, delayN });
       } else {
-        reject({ position, delay });
+        reject({ position, delayN });
       }
-    }, delay);
+    }, delayN);
   });
   return newPromise;
 }
+const form = document.querySelector('.form')
+
+function onSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = event.target;
+  let delayN = Number(delay.value);
+  let amountN = Number(amount.value);
+  let stepN = Number(step.value);
+
+  for (let position = 0; position < amountN; position += 1) {
+    createPromise({ position, delayN })
+      .then(({ position, delayN }) => {
+        Notiflix.Notify.success(
+          `Fulfilled promise ${position + 1} in ${delayN}ms`
+        );
+      })
+      .catch(({ position, delayN }) => {
+        Notiflix.Notify.failure(
+          `Rejected promise ${position + 1} in ${delayN}ms`
+        );
+      });
+    delayN += step;
+  }
+}
+form.addEventListener('submit', onSubmit);
